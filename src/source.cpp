e71c779c8fdd496bfed3a0acb14b8283dc850159
cheeseandmacaroni <nikitosik25565@mail.ru>
2021-10-29 20:42:30 +0300

    precision set

--- src/source.cpp
+++ src/source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <complex>
 #include <vector>
@@ -113,15 +114,15 @@ void mkl_fft(t_complex_vector& in, t_complex_vector &out)
 {
 	DFTI_DESCRIPTOR_HANDLE descriptor;
 	MKL_LONG status;
-	DFTI_CONFIG_VALUE presicion;
+	DFTI_CONFIG_VALUE precision;
 	DFTI_CONFIG_VALUE placement;
 	if (typeid(el_type) == typeid(double))
 	{
-		presicion = DFTI_CONFIG_VALUE::DFTI_DOUBLE;
+		precision = DFTI_CONFIG_VALUE::DFTI_DOUBLE;
 	}
 	else if (typeid(el_type) == typeid(float))
 	{
-		presicion = DFTI_CONFIG_VALUE::DFTI_SINGLE;
+		precision = DFTI_CONFIG_VALUE::DFTI_SINGLE;
 	}
 	if (&in == &out)
 	{
@@ -131,7 +132,7 @@ void mkl_fft(t_complex_vector& in, t_complex_vector &out)
 	{
 		placement = DFTI_CONFIG_VALUE::DFTI_NOT_INPLACE;
 	}
-	status = DftiCreateDescriptor(&descriptor, presicion, DFTI_COMPLEX, 1, in.size()); //Specify size and precision
+	status = DftiCreateDescriptor(&descriptor, precision, DFTI_COMPLEX, 1, in.size()); //Specify size and precision
 	status = DftiSetValue(descriptor, DFTI_PLACEMENT, placement); //In/out of place FFT
 	status = DftiCommitDescriptor(descriptor); //Finalize the descriptor
 	status = DftiComputeForward(descriptor, in.data(), out.data()); //Compute the Forward FFT
@@ -140,10 +141,11 @@ void mkl_fft(t_complex_vector& in, t_complex_vector &out)
 
 int main()
 {
-	int n = 8192 * 128;
+	int n = 32768;
 	double mkl_time;
 	double my_fft_time;
 	std::ofstream file_out("../out.xls");
+	file_out << std::fixed << std::showpoint << std::setprecision(3);
 	std::vector<std::complex<el_type>> x(n, std::complex<el_type>());
 	std::vector<std::complex<el_type>> y(n, std::complex<el_type>());
 	file_out << "size" << '\t' << "MKL_FFT" << '\t' << "MY_FFT" << std::endl;
@@ -151,7 +153,7 @@ int main()
 	{
 		y[i] = x[i] = std::complex<el_type>(rand() % 20 / 10. - 1, rand() % 20 / 10. - 1);
 	}
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 12; ++i)
 	{
 		mkl_time = omp_get_wtime();
 		mkl_fft(y, y);
